76215f79d7e763f6543296177efa70cd775922c9
Tim Butler <tbutler1132@gmail.com>
2026-01-02 17:26:59 -0500

    ignore next for check

--- src/apps/web/next.config.ts
+++ src/apps/web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["../../libs"],
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
 };
 
 export default nextConfig;
